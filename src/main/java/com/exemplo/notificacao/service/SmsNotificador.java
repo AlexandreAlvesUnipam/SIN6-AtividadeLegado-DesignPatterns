5fec4b8ce3d48dad860fee4f149caef1963a0f0b
Rafael Rodrigues <130514825+Rafael-V-B-Rodrigues@users.noreply.github.com>
2025-10-01 23:24:18 +0000

    fix: alterando sysout para logger

--- src/main/java/com/exemplo/notificacao/service/SmsNotificador.java
+++ src/main/java/com/exemplo/notificacao/service/SmsNotificador.java
@@ -1,12 +1,17 @@
 package com.exemplo.notificacao.service;
 
 import com.exemplo.notificacao.model.Pedido;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SmsNotificador implements Notificador {
+
+    private static final Logger logger = LoggerFactory.getLogger(SmsNotificador.class);
+
     @Override
     public void enviar(Pedido pedido) {
-        System.out.println("Enviando SMS para " + pedido.getCliente());
+        logger.info("Enviando SMS para {}", pedido.getCliente());
     }
 }
