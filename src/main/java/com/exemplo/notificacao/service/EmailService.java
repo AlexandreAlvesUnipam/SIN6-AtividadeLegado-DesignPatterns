9e5eac48050465fa9db046c248f8fea69a920de5
MaiaThePig <52360131+MaiaThePig@users.noreply.github.com>
2025-10-06 23:09:27 +0000

    refactor/Kaueh-GabrielMaia - Notificator

--- src/main/java/com/exemplo/notificacao/service/EmailService.java
+++ src/main/java/com/exemplo/notificacao/service/EmailService.java
@@ -2,10 +2,16 @@ package com.exemplo.notificacao.service;
 
 import com.exemplo.notificacao.model.Pedido;
 import org.springframework.stereotype.Service;
+import com.exemplo.notificacao.notifier.Notificador;
 
 @Service
-public class EmailService {
+public class EmailService implements Notificador {
+    @Override
     public void enviar(Pedido pedido) {
         System.out.println("Enviando e-mail para " + pedido.getCliente());
+    }
+    @Override
+    public String nome() {
+        return "email";
     }
 }
--- src/main/java/com/exemplo/notificacao/service/PushService.java
+++ src/main/java/com/exemplo/notificacao/service/PushService.java
@@ -2,10 +2,17 @@ package com.exemplo.notificacao.service;
 
 import com.exemplo.notificacao.model.Pedido;
 import org.springframework.stereotype.Service;
+import com.exemplo.notificacao.notifier.Notificador;
 
 @Service
-public class PushService {
+public class PushService implements Notificador {
+    @Override
     public void enviar(Pedido pedido) {
         System.out.println("Enviando push notification para " + pedido.getCliente());
+    }
+    
+    @Override
+    public String nome() {
+        return "push";
     }
 }
